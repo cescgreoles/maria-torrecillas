e245f09b085ce184be7d3cd66d399402a97093a2
cescgreoles <cesc.greoles@gmail.com>
2024-10-21 19:36:35 +0200

    modify next/config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["maria-torrecillas-pl4t-two.vercel.app"],
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
